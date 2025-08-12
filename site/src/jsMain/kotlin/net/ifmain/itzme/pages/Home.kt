734e717d79ea5fbae9784262bd2aa48b7e4e6871
gayoung <gayoung990911@gmail.com>
2025-08-12 13:49:41 +0900

    fix: Alignment import 누락 수정

--- site/src/jsMain/kotlin/net/ifmain/itzme/pages/Home.kt
+++ site/src/jsMain/kotlin/net/ifmain/itzme/pages/Home.kt
@@ -5,6 +5,7 @@ import com.varabyte.kobweb.compose.css.FontWeight
 import com.varabyte.kobweb.compose.foundation.layout.Box
 import com.varabyte.kobweb.compose.foundation.layout.Column
 import com.varabyte.kobweb.compose.foundation.layout.Row
+import com.varabyte.kobweb.compose.ui.Alignment
 import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.modifiers.*
 import com.varabyte.kobweb.compose.ui.styleModifier
